5222da32b955629e503a54ed9481f7ee16467b02
KARIᴴᵀᵀᴾ😌 <moneyheistofc@gmail.com>
2025-11-09 20:40:49 +0530

    Create Footer.tsx

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="site-footer">
+      © {new Date().getFullYear()} MR NIPUN OFC / TECH-WEB. All Rights Reserved.
+    </footer>
+  );
+}
