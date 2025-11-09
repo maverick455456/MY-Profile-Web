2268f095c8449fea168fda448e8184b6d6b6398b
KARIᴴᵀᵀᴾ😌 <moneyheistofc@gmail.com>
2025-11-09 20:19:20 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,17 +11,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* Icons (small CSS, lazy) */}
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-          media="print"
-          onLoad="this.media='all'"
-        />
-      </head>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  {/* Font Awesome (simple, type-safe) */}
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    crossOrigin="anonymous"
+    referrerPolicy="no-referrer"
+  />
+</head>
       <body>
         {children}
       </body>
