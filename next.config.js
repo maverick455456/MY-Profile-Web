86cbbdc2f9497f1b485429a4cb31b1b7872ceb5f
KARIᴴᵀᵀᴾ😌 <moneyheistofc@gmail.com>
2025-11-10 10:44:55 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'raw.githubusercontent.com' }
-    ]
-  }
+      { protocol: 'https', hostname: 'raw.githubusercontent.com' },
+    ],
+  },
 };
 module.exports = nextConfig;
